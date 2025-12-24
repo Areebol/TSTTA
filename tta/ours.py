Fix(leakage): data leakage in ours ci-loss adapter

tta/ours.py
@@ -92,6 +92,14 @@ class TTARunner(nn.Module):
             self.optimizer.zero_grad()
             loss.backward()
             self.optimizer.step()
+            
+            if isinstance(self.gating, CILossTrendGating):
+                se = F.mse_loss(pred_partial, ground_truth_partial, reduction='none')
+                per_channel_mse = se.mean(dim=(0, 1)) # 形状为 [n_vars]
+                self.gating.update_loss(per_channel_mse)
+            elif isinstance(self.gating, CGLossTrendGating):
+                mse_loss = F.mse_loss(pred_partial, ground_truth_partial)
+                self.gating.update_loss(mse_loss)
 
     @torch.no_grad()
     def _adjust_prediction(self, pred, inputs, batch_size, period):
@@ -155,12 +163,6 @@ class TTARunner(nn.Module):
 
                 # Forecast
                 pred, ground_truth = forecast(self.cfg, cur_inputs, self.model, None)
-                with torch.no_grad():
-                    if isinstance(self.gating, CILossTrendGating):
-                        se = F.mse_loss(pred, ground_truth, reduction='none')
-                        per_channel_mse = se.mean(dim=(0, 1)) # 形状为 [n_vars]
-                    elif isinstance(self.gating, CGLossTrendGating):
-                        mse_loss = F.mse_loss(pred, ground_truth)
                 self.all_preds_base.append(pred.detach().cpu().numpy())
                 # Adapter Forward (No Grad for evaluation first)
                 with torch.no_grad():
@@ -180,10 +182,6 @@ class TTARunner(nn.Module):
                 mae = F.l1_loss(pred_adapter, ground_truth, reduction='none').mean(dim=(-2, -1)).detach().cpu().numpy()
                 self.mse_all.append(mse)
                 self.mae_all.append(mae)
-                if isinstance(self.gating, CILossTrendGating):
-                    self.gating.update_loss(per_channel_mse)
-                elif isinstance(self.gating, CGLossTrendGating):
-                    self.gating.update_loss(mse_loss)
                 self.data_manager.collect(
                 inputs=cur_enc_window, 
                 base_pred=pred,