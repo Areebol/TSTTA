b31bba1132ce6d74965ddfd3ad7d128846c006c7
DengZeshuai <1321730442@qq.com>
2026-01-25 04:07:21 +0000

    eved

--- build_table.py
+++ build_table.py
@@ -208,18 +208,19 @@ if __name__ == "__main__":
         
     build_summary_table(
         input_dir="./results/", 
-        # input_dir='/wenzhiquan/dengzeshuai/codes/TSTTA_COBA/results/transfer_results_260124',
+        # input_dir='/wenzhiquan/dengzeshuai/codes/TSTTA_COBA/results/transfer_results_260125',
         output_csv="./results/final_tta_summary.csv", 
+        # output_csv="/wenzhiquan/dengzeshuai/codes/TSTTA_COBA/results/transfer_results_260125/final_tta_summary.csv",
         
         # 1. 筛选数据集
         # dataset_names=["ETTh1", "ETTh2", "ETTm1", "ETTm2", "exchange_rate", "weather"],
-        dataset_names=["ETTh1_2_ETTh2", "ETTm2_2_ETTm1"],
-        # dataset_names=["ETTh1_2_ETTh2", "ETTh2_2_ETTh1", "ETTm1_2_ETTm2", "ETTm2_2_ETTm1"],
+        # dataset_names=["ETTh1_2_ETTh2", "ETTm2_2_ETTm1"],
+        dataset_names=["ETTh1_2_ETTh2", "ETTh2_2_ETTh1", "ETTm1_2_ETTm2", "ETTm2_2_ETTm1"],
         # dataset_names=None,
         
         # 2. 筛选模型 (可以是列表，也可以是单个字符串)
-        # model_names=["DLinear", "PatchTST", "FreTS", "iTransformer", "MICN", "OLS", "PatchTST"], 
+        model_names=["DLinear", "PatchTST", "FreTS", "iTransformer", "MICN", "OLS", "PatchTST"], 
         # model_names="iTransformer"
-        model_names=["DLinear", "iTransformer"]
+        # model_names=["DLinear", "iTransformer"]
         # model_names=None # 设为 None 则显示所有模型
     )
